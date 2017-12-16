4b34064af860901287b394c02320a6d33d669f47
yonybrondy <yonybrondy17@gmail.com>
2017-12-16 09:25:41 -0500

    Change name of ExampleTest to EjemploTest

---
+++ tests/features/EjemploTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class EjemploTest extends FeatureTestCase
+{
+    function test_basic_example()
+    {
+        $user = factory(\App\User::class)->create([
+            "name" => "Yony Brondy",
+            "email" => "yonybrondy17@gmail.com",
+        ]);
+        $this->actingAs($user,'api')
+            ->visit('api/user')
+             ->see("Yony Brondy")
+              ->see("yonybrondy17@gmail.com");
+    }
+}
